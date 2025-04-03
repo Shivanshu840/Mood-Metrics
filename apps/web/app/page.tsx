893e52ae2cc607ed312ff5c79ebf093642af52f7
Shivanshu840 <rajshivanshu544@gmail.com>
2025-04-03 22:46:48 +0530

    remove the unused variable

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,3 @@
-import { SidebarProvider } from "@repo/ui/sidebar"
-import { MoodMetricsSidebar } from "../components/mood-metrics"
-import { DashboardContent } from "@/components/dashboard-content"
-
 import LoginPage from "@/components/login"
 
 export default function Home() {
